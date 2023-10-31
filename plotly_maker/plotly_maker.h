e6cc2009370d543fe3673767ea3ea6d6f3a9d4da
Valery <tor-tech@mail.ru>
2023-10-31 21:39:21 +0300

    Plotly maker (#7)
    * create string values draft function
    
    * add function for creating html page with plotly js
    
    * plotly maker test

--- plotly_maker/plotly_maker.h
+++ plotly_maker/plotly_maker.h
@@ -1,11 +1,15 @@
 #ifndef PLOTLY_MAKER_PLOTLY_MAKER_H_
 #define PLOTLY_MAKER_PLOTLY_MAKER_H_
 
+#include "vector"
+#include "string"
+using std::vector;
+
 namespace davis {
 
-bool testPlottyMaker();
+bool createHtmlPageWithPlotlyJS(const vector<vector<double>> &values, std::string &page);
 
-}; // namespace pm
+}; // namespace davis
 
 #endif // PLOTLY_MAKER_PLOTLY_MAKER_H_
 
