341f99fc53c9e60b1ff173b44ba79778a796b6b0
AntonMrt <antonmartenov@gmail.com>
2023-11-02 00:54:14 +0300

    show functions some work

--- array_core/array_core.h
+++ array_core/array_core.h
@@ -4,36 +4,87 @@
 #include <iostream>
 #include <vector>
 #include "plotly_maker/plotly_maker.h"
+#include "utils_core.h"
+
+using std::vector;
+
 
 namespace davis {
 
-bool generateAndShowHtml(std::vector<std::vector<double>> array);
+enum class dataType{
+    ptr1D, ptr2D, ptrPseudo2D, vector2D, vector1D
+};
 
+bool generateAndShowHtml(const vector<vector<double>> &data);
 
+//! two-dimensional vector
 template <typename T>
-void show(std::vector<std::vector<T>> array);
+void show(const vector<vector<T>> &data);
 
+//! one-dimensional vector
 template <typename T>
-void show(std::vector<T> array);
+void show(const vector<T> &data);
 
 //! two-dimensional array
 template <typename T>
-void show(T **array, uint64_t arrRows, uint64_t arrCols);
+void show(const T **data, uint64_t arrRows, uint64_t arrCols);
 
-//! A one-dimensional array that simulates a two-dimensional one (element access [i*cols+j])
+//! a one-dimensional array that simulates a two-dimensional one (element access [i*cols+j])
 template <typename T>
-void show(T **array, uint64_t arrRows, uint64_t arrCols);
+void show(const T *data, uint64_t arrRows, uint64_t arrCols);
 
-//! two-dimensional array
+//! one-dimensional array
 template <typename T>
-void show(T *array, uint64_t size);
+void show(const T *data);
+
+// *******************************
+// template functions realisations:
+// *******************************
 
 template <typename T>
-void show(std::vector<std::vector<T>> array){
-    std::cout<< "show"<<std::endl;
+void show(const vector<vector<T>> &data)
+{
+    dataType type = dataType::vector2D;
+    vector<vector<double>> vecVecDbl;
+    vecVecDbl.reserve(data.size());
+    for (vector<T> row : data) {
+        vector<double> dblRow(row.begin(), row.end());
+        vecVecDbl.push_back(dblRow);
+    }
+}
+
+template <typename T>
+void show(const vector<T> &data)
+{
+    dataType type = dataType::vector1D;
+    vector<double> dblRow(data.begin(), data.end());
+
+}
+
+template <typename T>
+void show(const T **data, uint64_t arrRows, uint64_t arrCols)
+{
+    dataType type = dataType::ptr2D;
+    //TODO
+}
+
+template <typename T>
+void show(const T *data, uint64_t arrRows, uint64_t arrCols)
+{
+    dataType type = dataType::ptrPseudo2D;
+    //TODO
+}
+
+template <typename T>
+void show(const T *data, uint64_t count)
+{
+    dataType type = dataType::ptr1D;
+    vector<double> values(data, data + count);
+
 }
 
 } // namespace davis
+
 
 
 
---
+++ array_core/utils_core.h
@@ -0,0 +1,12 @@
+#ifndef ARRAY_CORE_UTILS_CORE_H
+#define ARRAY_CORE_UTILS_CORE_H
+
+namespace utilsCore {
+
+
+} // namespace utils_core
+
+
+
+
+#endif // ARRAY_CORE_UTILS_CORE_H
