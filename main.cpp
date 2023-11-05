992e13abf9cffbd980c3fd9f29a9fa5b5e38d4c2
Valery <tor-tech@mail.ru>
2023-11-06 00:41:49 +0300

    add preprocessors directive to function

--- main.cpp
+++ main.cpp
@@ -16,15 +16,14 @@ int main(int argc, char *argv[])
         davis::saveStringToFile("plotly-2.27.0.min.js",resource_handle.c_str());
     }
 
-
     // example how to show values via ArrayCore using PlotlyMaker lib
     std::vector<std::vector<int>> values = {{300,40,98,76},{99,45,20,1},{5,56,93,25},{45,23,90,2}};
     davis::show(values);
     davis::sleepMs(2000);
 
     //line chart
-    std::vector<double>values3 = {30,50,456,98,23,128,98};
-    davis::showLineChartInBrowser(values3);
+    std::vector<double>values2 = {30,50,456,98,23,128,98};
+    davis::showLineChartInBrowser(values2);
     return EXIT_SUCCESS;
 }
 
