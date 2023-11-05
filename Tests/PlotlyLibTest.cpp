f5dc417b6737d48c35c5496152b7be65027f499d
Valery <tor-tech@mail.ru>
2023-11-05 13:06:14 +0300

    WIP (add resource manager to tests)

--- Tests/PlotlyLibTest.cpp
+++ Tests/PlotlyLibTest.cpp
@@ -8,7 +8,7 @@ using std::string;
 
 
 TEST(PlotlyMaker, CreateDefaultHeatMapHtmlPageTest) {
-    //TODO delete example.html before this test
+
     std::vector<std::vector<double>>testValues = {{43,400,54,980},{200,36,400,55},{120,4,650,5}};
     std::string str_page;
     bool result = davis::createHtmlPageWithPlotlyJS(testValues, str_page);
@@ -25,12 +25,7 @@ TEST(PlotlyMaker, CreateDefaultHeatMapHtmlPageTest) {
 }
 
 TEST(ArrayCore, showTests){
-    ResourceHandle resource_handle("plotly_maker/plotly-2.27.0.min.js");
-    if(!davis::isPlotlyScriptExists()){
-        davis::saveStringToFile("plotly-2.27.0.min.js",resource_handle.c_str());
-    }
-
-
+    EXPECT_EQ(davis::isPlotlyScriptExists(),true);
     //// 1
     std::vector<std::vector<double>> values = {{300.3,40,98,76},{99,45,20,1},{5,56,93,25},{45,23,90,2}};
     bool result = davis::show(values);
@@ -77,7 +72,10 @@ TEST(ArrayCore, showTests){
 
 
 int main(int argc, char* argv[]){
-
+    ResourceHandle resource_handle("res/plotly-2.27.0.min.js");
+    if(!davis::isPlotlyScriptExists()){
+        davis::saveStringToFile("plotly-2.27.0.min.js",resource_handle.c_str());
+    }
     ::testing::InitGoogleTest(&argc, argv);
     std::ignore = RUN_ALL_TESTS();
     return 0;
