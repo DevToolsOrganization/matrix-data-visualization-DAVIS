35e2af22f380c24386143680d9fbd66ea28f7039
Valery <tor-tech@mail.ru>
2023-11-13 13:26:27 +0300

    Revert "Change format to chromium style (#28)"
    This reverts commit 6fb288a80e7cfb2ff996701161fec84deff949b2.

--- Tests/ArrayCoreTest.cpp
+++ Tests/ArrayCoreTest.cpp
@@ -7,59 +7,59 @@
 using std::string;
 
 TEST(ArrayCore, showHeatMap1) {
-  EXPECT_EQ(davis::isPlotlyScriptExists(), true);
-  std::vector<std::vector<double>> values = {{30.3, 40, 98, 76}, {99, 45, 20, 1}, {5, 56, 93, 25}, {45, 23, 90, 2}};
-  bool result = davis::show(values, "test1");
-  EXPECT_EQ(result, true);
+    EXPECT_EQ(davis::isPlotlyScriptExists(), true);
+    std::vector<std::vector<double>> values = {{30.3, 40, 98, 76}, {99, 45, 20, 1}, {5, 56, 93, 25}, {45, 23, 90, 2}};
+    bool result = davis::show(values, "test1");
+    EXPECT_EQ(result, true);
 }
 
 TEST(ArrayCore, showHeatMap2) {
 // davis::sleepMs(1000);
-  std::vector<int> vals2 = {2, 6, 4, 34, 56, 33, 2, 1};
-  bool result = davis::show(vals2, "test2");
-  EXPECT_EQ(result, true);
+    std::vector<int> vals2 = {2, 6, 4, 34, 56, 33, 2, 1};
+    bool result = davis::show(vals2, "test2");
+    EXPECT_EQ(result, true);
 }
 
 TEST(ArrayCore, showHeatMap3) {
 // davis::sleepMs(1000);
-  int vals3[] = {2, 6, 4, -34, 56, 33, 2, 15};
-  bool result = davis::show(vals3, sizeof(vals3) / sizeof(vals3[0]), "test3");
-  EXPECT_EQ(result, true);
+    int vals3[] = {2, 6, 4, -34, 56, 33, 2, 15};
+    bool result = davis::show(vals3, sizeof(vals3) / sizeof(vals3[0]), "test3");
+    EXPECT_EQ(result, true);
 }
 
 TEST(ArrayCore, showHeatMap4) {
 // davis::sleepMs(1000);
-  int rows = 5;
-  int cols = 3;
-  int* vals4 = new int[rows * cols];
-  for (int i = 0; i < rows; ++i) {
-    for (int j = 0; j < cols; ++j) {
-      vals4[i * cols + j] = i * cols + j;
+    int rows = 5;
+    int cols = 3;
+    int* vals4 = new int[rows * cols];
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            vals4[i * cols + j] = i * cols + j;
+        }
     }
-  }
-  bool result = davis::show(vals4, rows, cols, "test4");
-  EXPECT_EQ(result, true);
+    bool result = davis::show(vals4, rows, cols, "test4");
+    EXPECT_EQ(result, true);
 }
 
 TEST(ArrayCore, showHeatMap5) {
 // davis::sleepMs(1000);
-  int rows2 = 20;
-  int cols2 = 20;
-  int** vals5 = new int* [rows2];
-  for (int i = 0; i < rows2; ++i) {
-    vals5[i] = new int[cols2];
-    for (int j = 0; j < cols2; ++j) {
-      vals5[i][j] = i * cols2 + j;
+    int rows2 = 20;
+    int cols2 = 20;
+    int** vals5 = new int* [rows2];
+    for (int i = 0; i < rows2; ++i) {
+        vals5[i] = new int[cols2];
+        for (int j = 0; j < cols2; ++j) {
+            vals5[i][j] = i * cols2 + j;
+        }
     }
-  }
-  bool result = davis::show(vals5, rows2, cols2);
-  EXPECT_EQ(result, true);
+    bool result = davis::show(vals5, rows2, cols2);
+    EXPECT_EQ(result, true);
 }
 
 
 int main(int argc, char* argv[]) {
-  //std::cout<< std::endl << davis::deleteFolder(davis::saveFolderName) << std::endl;
-  ::testing::InitGoogleTest(&argc, argv);
-  std::ignore = RUN_ALL_TESTS();
-  return 0;
+    //std::cout<< std::endl << davis::deleteFolder(davis::saveFolderName) << std::endl;
+    ::testing::InitGoogleTest(&argc, argv);
+    std::ignore = RUN_ALL_TESTS();
+    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -10,40 +10,40 @@
 
 
 int main(int argc, char* argv[]) {
-  cxxopts::Options options("davis", "data visualization utility");
-  options.add_options()
-         ("l,linechart", "linechart values", cxxopts::value<std::string>())
-         ("m,heatmap", "heatmap values", cxxopts::value<std::string>())
-         ("h,help", "davis commands")
-         ;
-  auto result = options.parse(argc, argv);
-  if (result.count("help")) {
-    std::cout << options.help() << std::endl;
-    exit(0);
-  }
-  // here we have to copy plotly js to current dir if it doesn't exist
-  ResourceHandle resource_handle(davis::kPlotlyJsResourcePath);
-  if (!davis::isPlotlyScriptExists()) {
-    davis::mayBeCreateJsWorkingFolder();
-    davis::saveStringToFile(davis::kPlotlyJsWorkPath, resource_handle.c_str());
-  }
-  if (result.count("linechart")) {
-    auto data = result["linechart"].as<std::string>();
-    davis::showLineChartInBrowser(data, "comand_line_linechart", davis::showSettings());
+    cxxopts::Options options("davis", "data visualization utility");
+    options.add_options()
+           ("l,linechart", "linechart values", cxxopts::value<std::string>())
+           ("m,heatmap", "heatmap values", cxxopts::value<std::string>())
+           ("h,help", "davis commands")
+           ;
+    auto result = options.parse(argc, argv);
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        exit(0);
+    }
+    // here we have to copy plotly js to current dir if it doesn't exist
+    ResourceHandle resource_handle(davis::kPlotlyJsResourcePath);
+    if(!davis::isPlotlyScriptExists()) {
+        davis::mayBeCreateJsWorkingFolder();
+        davis::saveStringToFile(davis::kPlotlyJsWorkPath, resource_handle.c_str());
+    }
+    if(result.count("linechart")) {
+        auto data = result["linechart"].as<std::string>();
+        davis::showLineChartInBrowser(data, "comand_line_linechart", davis::showSettings());
+        return EXIT_SUCCESS;
+    }
+    if(result.count("heatmap")) {
+        auto data = result["heatmap"].as<std::string>();
+        davis::showHeatMapInBrowser(data, "comand_line_heatmap", davis::showSettings());
+        return EXIT_SUCCESS;
+    }
+    // example how to show values via ArrayCore using PlotlyMaker lib
+    std::vector<std::vector<int>> values = {{300, 40, 98, 76}, {99, 45, 20, 1}, {5, 56, 93, 25}, {45, 23, 90, 2}};
+    davis::show(values, "values");
+    //line chart
+    std::vector<double>values2 = {30, 50, 456, 98, 23, 128, 98};
+    davis::show(values2, "values2");
     return EXIT_SUCCESS;
-  }
-  if (result.count("heatmap")) {
-    auto data = result["heatmap"].as<std::string>();
-    davis::showHeatMapInBrowser(data, "comand_line_heatmap", davis::showSettings());
-    return EXIT_SUCCESS;
-  }
-  // example how to show values via ArrayCore using PlotlyMaker lib
-  std::vector<std::vector<int>> values = {{300, 40, 98, 76}, {99, 45, 20, 1}, {5, 56, 93, 25}, {45, 23, 90, 2}};
-  davis::show(values, "values");
-  //line chart
-  std::vector<double>values2 = {30, 50, 456, 98, 23, 128, 98};
-  davis::show(values2, "values2");
-  return EXIT_SUCCESS;
 }
 
 
